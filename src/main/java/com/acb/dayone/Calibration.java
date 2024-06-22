63dd872469f5987485d72f1036841e0055a1cec0
Anil Can Baykal <anilcan.baykal@kfzteile24.de>
2024-06-22 16:54:09 +0200

    day 1.2

--- src/main/java/com/acb/dayone/Calibration.java
+++ src/main/java/com/acb/dayone/Calibration.java
@@ -8,36 +8,61 @@ import static com.acb.aux.Util.base10;
 /**
  * Created on 21.06.24.
  */
-public class Calibration {
+public record Calibration(List<String> input) {
 
-    public List<String> input;
+    public static final List<String> DIGITS = List.of("one", "two", "three", "four", "five", "six", "seven", "eight", "nine");
 
-    public Calibration(List<String> input) {
-        this.input = input;
+
+    public static String digitize(final String line) {
+
+        String input = line;
+        if (input.isEmpty()) return line;
+        for (int j = 0; j < DIGITS.size(); j++) {
+            if (input.startsWith(DIGITS.get(j))) {
+                // twoXXX -> 2XX is wrong
+                // twoXXX -> 2woXX is correct
+
+               /// input = input.replaceFirst(DIGITS.get(j), String.valueOf(j + 1));
+                return String.valueOf(j + 1) + digitize(input.substring(1));
+            }
+        }
+
+        return input.charAt(0) + digitize(input.substring(1));
     }
 
-    public static int[] firstAndLast(final String line){
+    public static int[] firstAndLast(final String line) {
         assert !line.isEmpty();
 
         final int[] array = line.chars().filter(x -> x >= 48 && x <= 57)
                 .map(x -> x - 48).toArray();
 
-        if (array.length > 1){
-            return new int[]{array[0], array[array.length - 1]};
+        if (array.length > 1) {
+            return new int[] {array[0], array[array.length - 1]};
         } else {
             return new int[] {array[0], array[0]};
         }
     }
 
-    public static Integer lineSum(final String line){
+    public static Integer lineSum(final String line) {
         assert !line.isEmpty();
-
         final int[] array = firstAndLast(line);
         return base10(Arrays.stream(array).boxed().toList());
     }
 
-    public int sum(){
+    public int[] sumByLine(){
+        return input.stream().map(Calibration::digitize).map(Calibration::lineSum).mapToInt(Integer::intValue).toArray();
+    }
+    public int sumOne() {
         return input.stream().map(Calibration::lineSum).reduce(Integer::sum).orElse(0);
+    }
+
+
+    public int sumTwo() {
+        return input.stream().map(Calibration::digitize).map(Calibration::lineSum).reduce(Integer::sum).orElse(0);
+    }
+
+    public void log(){
+        input.stream().peek(x-> System.out.printf("%-40s%-40s%-40s\n", x, digitize(x), lineSum(digitize(x)))).toArray();
     }
 
 
--- src/test/java/com/acb/dayone/CalibrationTest.java
+++ src/test/java/com/acb/dayone/CalibrationTest.java
@@ -15,12 +15,9 @@ class CalibrationTest {
 
     @Test
     void lineSum() {
-    /*1abc2
-    pqr3stu8vwx
-    a1b2c3d4e5f
-    treb7uchet*/
 
-        assertArrayEquals(new int[] {1}, Calibration.firstAndLast("1"));
+
+        assertArrayEquals(new int[] {1,1}, Calibration.firstAndLast("1"));
         assertArrayEquals(new int[] {1, 2}, Calibration.firstAndLast("12"));
         assertArrayEquals(new int[] {1, 2}, Calibration.firstAndLast("132"));
         assertArrayEquals(new int[] {1, 2}, Calibration.firstAndLast("1a3bc2"));
@@ -32,14 +29,38 @@ class CalibrationTest {
     }
 
     @Test
+    void digitizeTest(){
+        assertEquals("123", Calibration.digitize("1two3"));
+        assertEquals("123", Calibration.digitize("1twothree"));
+        assertEquals("234", Calibration.digitize("twothreefour"));
+        assertEquals("2340", Calibration.digitize("twothreefour0"));
+
+        assertEquals("8wo3", Calibration.digitize("eightwothree"));
+        assertArrayEquals(new int[]{8,3}, Calibration.firstAndLast(Calibration.digitize("eightwothree")));
+        assertArrayEquals(new int[]{6,8}, Calibration.firstAndLast(Calibration.digitize("69xvbxfffmr7one8gmch8")));
+        assertArrayEquals(new int[]{7,8}, Calibration.firstAndLast(Calibration.digitize("kqxncc74sjttqklx3lxpffbdlthreetwooneightnb")));
+    }
+
+    @Test
     void sum() {
         final Calibration calibration = new Calibration(List.of("1abc2", "pqr3stu8vwx", "a1b2c3d4e5f", "treb7uchet"));
-        assertEquals(142, calibration.sum());
+        assertEquals(142, calibration.sumOne());
+
+        final Calibration calibration2 = new Calibration(Util.readStrings("/day1/sample.txt"));
+        assertArrayEquals(new int[] {29, 83, 13, 24, 42, 14,76}, calibration2.sumByLine());
+        assertEquals(281, calibration2.sumTwo());
     }
 
     @Test
     void sumOne() {
         final Calibration calibration = new Calibration(Util.readStrings("/day1/input1.txt"));
-        assertEquals(54239, calibration.sum());
+        assertEquals(54239, calibration.sumOne());
+    }
+
+    @Test
+    void sumTwo() {
+        final Calibration calibration = new Calibration(Util.readStrings("/day1/input1.txt"));
+        calibration.log();
+        assertEquals(55343, calibration.sumTwo());
     }
 }
